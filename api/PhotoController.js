d6f17700943f914a435c5e778d3378a65ceb453a
Sami Triki <contact@triki.io>
2015-08-19 16:53:49 +0100

    feat - (api): cleanup

--- api/PhotoController.js
+++ api/PhotoController.js
@@ -1,10 +1,5 @@
-var restify = require('restify');
-var server = restify.createServer();
-
 var retrieve = require('./utils/retrieve');
 var conf = require('./config.js');
-
-server.use(restify.bodyParser());
 
 exports.init = function(req, res, next) {
     retrieve.directoryList(conf.directory).then(function(list) {
@@ -16,23 +11,16 @@ exports.init = function(req, res, next) {
 
 exports.pick = function (req, res, next) {
     retrieve.directoryList(conf.directory).then(function(list) {
-        console.log('index', parseInt(req.params.index));
-        res.send(retrieve.pick(list, parseInt(req.params.index)));
+        var index = (req.params.index >= list.nbFiles - 1) ? -1 : parseInt(req.params.index);
+        res.send(retrieve.pick(list, index)); // copies current and returns next image in queue
         next();
     });
 };
 
 exports.pass = function (req, res, next) {
     retrieve.directoryList(conf.directory).then(function(list) {
-        res.send(retrieve.pass(list, parseInt(req.params.index)));
+        var index = (req.params.index >= list.nbFiles - 1) ? -1 : parseInt(req.params.index);
+        res.send(retrieve.pass(list, index)); // returns next image in queue
         next();
     });
 };
-
-server.get('/photo', exports.init);
-server.post('/photo/pick', exports.pick);
-server.post('/photo/pass', exports.pass);
-
-server.listen(8080, function() {
-  console.log('%s listening at %s', server.name, server.url);
-});
--- api/app.js
+++ api/app.js
@@ -1,4 +1,13 @@
 var restify = require('restify');
 var server = restify.createServer();
-var Photo = require('PhotoController');
+var Photo = require('./PhotoController');
 
+server.use(restify.bodyParser());
+
+server.get('/photo', Photo.init);
+server.post('/photo/pick', Photo.pick);
+server.post('/photo/pass', Photo.pass);
+
+server.listen(8080, function() {
+  console.log('%s listening at %s', server.name, server.url);
+});
